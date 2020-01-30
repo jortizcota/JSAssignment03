85446fe2ce6d2326b4ec771499341a1d46484b13
Ortiz Cota <josephortizcota@my.whccd.edu>
2020-01-29 21:17:06 -0800

    Answered my third question on assignment 03.

--- script3.js
+++ script3.js
@@ -10,19 +10,13 @@
 // # # # #
 //  # # # #
 
-let size = 8;
-
-let board = "";
-
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
-      board += "#";
-    } else {
-      board += " ";
-    }
+let i = 1;
+while(i <= 8) {
+  if(i % 2 == 0) {
+    console.log(" # # # #");
   }
-  board += "\n";
+  else{
+    console.log("# # # #");
+  }
+  i++;
 }
-
-console.log(board);
